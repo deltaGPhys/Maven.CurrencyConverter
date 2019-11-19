d4eb97e13fdab4c12506834a2f36f1531ea3dd1d
deltagphys <cty.jgates@gmail.com>
2019-11-19 10:00:33 -0500

    done

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -1,30 +1,45 @@
 package io.zipcoder.currencyconverterapplication;
 
 public enum CurrencyType {
-    AUSTRALIAN_DOLLAR(2.70),
-    CANADIAN_DOLLAR(2.64),
-    CHINESE_YR(13.84),
-    EURO(1.88),
-    FRANC(2.02),
-    POUND(1.64),
-    RINGGIT(8.94),
-    RUPEE(136.64),
-    SINGAPORE_DOLLAR(1.86),
-    US_DOLLAR(2.0),
-    UNIVERSAL_CURRENCY(1.0),
-    YEN(231.68);
+    AUSTRALIAN_DOLLAR(2.70,"AustralianDollar"),
+    CANADIAN_DOLLAR(2.64, "CanadianDollar"),
+    CHINESE_YR(13.84, "ChineseYR"),
+    EURO(1.88, "Euro"),
+    FRANC(2.02, "Franc"),
+    POUND(1.64, "Pound"),
+    RINGGIT(8.94, "Ringgit"),
+    RUPEE(136.64, "Rupee"),
+    SINGAPORE_DOLLAR(1.86, "SingaporeDollar"),
+    US_DOLLAR(2.0, "USDollar"),
+    UNIVERSAL_CURRENCY(1.0, "UniversalCurrency"),
+    YEN(231.68, "Yen");
 
     private final double rate;
+    private final String className;
 
-    CurrencyType(double rate) {
+    CurrencyType(double rate, String className) {
         this.rate = rate;
+        this.className = className;
     }
 
     public Double getRate() {
         return rate;
     }
 
-    public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
+    public String getClassName() {
+        return className;
+    }
+
+    public static CurrencyType getByClassName(String className) {
+        for (CurrencyType ct : CurrencyType.values()) {
+            if (ct.className.equals(className)) {
+                return ct;
+            }
+        }
         return null;
+    }
+
+    public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
+        return getByClassName(currency.getClass().getSimpleName());
     }
 }
